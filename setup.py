73aef2fc07cf5b9e88f3a824ee1baff5318da496
Kevin Lu <kevinyihchyunlu@gmail.com>
2024-12-15 15:59:55 +0800

    update opendal to >=0.45.2 to support newer versions of python

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
         "boto3>=1.24.89",
         "tqdm==4.64.0",
         "pyOpenSSL==22.1.0",
-        "opendal==0.45.2",
+        "opendal>=0.45.2",
         "asgiref==3.8.1"
     ],
     extras_require={
