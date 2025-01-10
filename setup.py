452cc9b821ee3a8465ed5ce498a824e2a2e327a5
Kevin Lu <kevinyihchyunlu@gmail.com>
2025-01-10 11:22:11 +0000

    fix: upgrade opendal to 0.45.14 and fix copy implementation

--- setup.py
+++ setup.py
@@ -13,8 +13,7 @@ setup(
         "boto3>=1.24.89",
         "tqdm==4.64.0",
         "pyOpenSSL==22.1.0",
-        "opendal>=0.45.2",
-        "smart_open==7.1.0",
+        "opendal@https://test-files.pythonhosted.org/packages/29/ea/66243458f4c5feca24d2434265743fe16ba83e4a61dfb93726828e4646fd/opendal-0.45.14-cp311-abi3-manylinux_2_28_aarch64.whl",  # noqa: E501
         "asgiref==3.8.1",
     ],
     extras_require={
